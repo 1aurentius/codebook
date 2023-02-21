09000d1e02ae2a1ea1ac922fb6164807543fef9f
Krister Alasaarela <krister.alasaarela@tuni.fi>
2023-02-21 10:50:27 +0200

    new branch pull request test

--- git.py
+++ git.py
@@ -1,1 +1,23 @@
-print('Hello world!')
+import requests
+
+# Replace these values with your own
+owner = '1aurentius'
+repo = 'codebook'
+pull_number = 1
+
+# Get the pull request URL
+url = f'https://api.github.com/repos/{owner}/{repo}/pulls/{pull_number}'
+response = requests.get(url)
+data = response.json()
+head_branch_url = data['head']['repo']['url']
+
+# Get the code from the pull request
+url = f'{head_branch_url}/pulls/{pull_number}/files'
+response = requests.get(url)
+data = response.json()
+
+# Print the contents of each file
+for file in data:
+    contents_url = file['raw_url']
+    response = requests.get(contents_url)
+    print(response.text)
